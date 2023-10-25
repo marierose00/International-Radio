5acaf9ade1261ca7337cbb3e835db0e7cb7eaf4a
marierose00 <148933655+marierose00@users.noreply.github.com>
2023-10-26 01:36:53 +0700

    Delete src directory

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import { Button } from "./buttons/button";
-import { Card } from "./cards/card";
-import { RadioPLayer } from "./media/player";
-
-export {
-    Button,
-    Card,
-    RadioPLayer
-}
